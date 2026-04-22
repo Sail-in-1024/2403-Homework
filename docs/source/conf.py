e8f243a241783c476ea9b12bf0282754595fabb6
Sail-in-1024 <Sail-ys1@outlook.com>
2026-04-22 22:06:06 +0800

    更新 conf.py
    改版本为新的预发布版本

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@ copyright = '2026, 颜赏'
 author = '颜赏'
 
 release = '1.1'
-version = '1.1.0b1'
+version = '1.1.0rc1'
 
 # -- General configuration
 
@@ -32,7 +32,9 @@ today_fmt = '%b %d, %Y (%H:%M %Z)'
 
 rst_epilog = '''
 .. |class-name| replace:: 803
-.. |page-source| replace:: *来自 https://2403-homework.readthedocs.io/ ，PDF格式可以在网页下载*
+.. |page-source| replace:: 
+   
+   .. [#p-s] *来自 https://2403-homework.readthedocs.io/ ，PDF格式可以在网页下载*
 '''
 
 # -- Options for HTML output
