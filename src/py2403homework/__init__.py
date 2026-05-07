022f63293c804a84fd67ea6f18a7ed5b2bb95630
Sail-in-1024 <Sail-ys1@outlook.com>
2026-05-07 20:46:29 +0800

    更新 __init__.py

--- src/py2403homework/__init__.py
+++ src/py2403homework/__init__.py
@@ -1,1 +1,7 @@
+"""
+用于生成 reStructuredText 文档源文件的模块。这在用于生成 2403的作业 时会很好用。
+
+A module used to generate reStructuredText source files of documentation. It must be helpful when generating 2403 Homework.
+"""
+
 from ._types import *
