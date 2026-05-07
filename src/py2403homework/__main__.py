39d9895903f8d50e3e71d191543e8795085b7a5a
Sail-in-1024 <Sail-ys1@outlook.com>
2026-05-07 22:33:56 +0800

    更新 __main__.py

--- src/py2403homework/__main__.py
+++ src/py2403homework/__main__.py
@@ -1,24 +1,8 @@
 import sys
-import argparse
 try:
-    from . import _types
+    from .commands import run
 except ImportError:
-    import _types
-
-
-parser = argparse.ArgumentParser(
-    # prog='python -m py2403homework',
-    description = 'A tool to generate reST content for homework',
-)
-parser.add_argument('-t', '--type', default='Homework')
-
-def run(args):
-    namespace = parser.parse_args(args)
-    try:
-        event_type = getattr(_types, namespace.type)
-    except AttributeError:
-        raise ValueError(f'event type "{namespace.type}" not found')
-    print(event_type())
+    from commands import run
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
